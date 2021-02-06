bdf70032339077b9352634271727eb2dac6b0d51
Ambuj Dubey <ambujdubey89@gmail.com>
2021-02-06 07:26:24 +0530

    Delete messages.js

--- api/config/messages.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "error": "Something went wrong, contact your administrator",
-  "dataNotFound": "Data not found",
-  "activeLicence": "Active licence data fetched successfully",
-}  
